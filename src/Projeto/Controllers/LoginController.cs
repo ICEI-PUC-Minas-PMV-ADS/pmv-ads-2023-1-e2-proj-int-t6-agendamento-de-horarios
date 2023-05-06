9ce31ce1aad96f4596518df2200def624ec989a7
MatheusDelay <matheus_delaysilva@hotmail.com>
2023-05-06 19:03:01 -0300

    Adicionado Login funcional

---
+++ src/Projeto/Controllers/LoginController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;
+using System.Security.Claims;
+
+namespace WebApplication1.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                return Json(new { Msg = "Usuário já logado!" });
+            }
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Logar(string email, string senha)
+        {
+            MySqlConnection mySqlConnection = new MySqlConnection("server=localhost;database=usuariosdb;uid=root;password=1234");
+            await mySqlConnection.OpenAsync();
+
+            MySqlCommand mySqlCommand = mySqlConnection.CreateCommand();
+            mySqlCommand.CommandText = $"SELECT * FROM usuarios Where email = '{email}' and senha = '{senha}'";
+
+            MySqlDataReader reader = mySqlCommand.ExecuteReader();
+
+            if (await reader.ReadAsync())
+            {
+                int usuarioId = reader.GetInt32(0);
+                string nome = reader.GetString(1);
+
+                return Json(new { Msg = "Usuario Logado com sucesso!" });
+            }
+
+            return Json(new { Msg = "Usuario nao encontrado" });
+
+        }
+        public async Task<IActionResult> Logout()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                await HttpContext.SignOutAsync();
+            }
+            return RedirectToAction("Index", "Login");
+        }
+    }
+}
---
+++ src/Projeto/Models/Usuario.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApplication1.Models
+{
+    [Table("Usuarios")]
+    public class Usuario
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Digite o nome de usuário")]
+        public string Nome { get; set; }
+        [Required(ErrorMessage = "Digite um e-mail válido")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Digite uma senha válida")]
+        public string Senha { get; set; }
+        [Required(ErrorMessage = "Digite um telefone para contato")]
+        public int Telefone { get; set; }
+
+        public Perfil Perfil { get; set; }
+    }
+    public enum Perfil
+    {
+        Admin,
+        Funcionario,
+        Cliente
+    }
+}
