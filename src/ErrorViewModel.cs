3e00b85988152d39e9b4c7393ca4fdcf7393f57c
Samuel Oliveira <112700596+sammueloliveira@users.noreply.github.com>
2023-05-25 15:04:39 -0300

    Delete src directory

--- src/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Barbearia.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
