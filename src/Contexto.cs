fd8f476492367eaf225c16e4688e4c9d28513368
Samuel Oliveira <112700596+sammueloliveira@users.noreply.github.com>
2023-05-25 14:59:53 -0300

    Add files via upload

---
+++ src/Contexto.cs
@@ -0,0 +1,17 @@
+﻿using Barbearia.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Barbearia.Data
+{
+    public class Contexto : IdentityDbContext
+    {
+
+        public Contexto(){}
+        public Contexto(DbContextOptions<Contexto> options)
+            : base(options)
+        {
+        }
+        public DbSet<Agendamentos> Agendamentos { get; set; }
+    }
+}
---
+++ src/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Barbearia.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
